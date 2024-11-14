569653023e45b86cd94c5f83e136dbf10b28790a
QTVxDevil <quyettv1302@gmail.com>
2024-11-15 00:39:05 +0700

    test webcam

--- WebApp/main.py
+++ WebApp/main.py
@@ -31,6 +31,7 @@ def delete(row_id):
     dtb.commit()
     
 def generate_frames():
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     camera = cv2.VideoCapture(0)
     while True:
         success, frame = camera.read()
@@ -39,6 +40,18 @@ def generate_frames():
         else:
             frame = cv2.flip(frame, 1)
             
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Detect faces in the frame
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+            # Draw rectangles and labels around each detected face
+            for (x, y, w, h) in faces:
+                # Draw a rectangle around the face
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Blue rectangle with thickness of 2
+                
+                # Add a label "Person" above the rectangle
+                cv2.putText(frame, 'Person', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
             # Encode the flipped frame in JPEG format
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
